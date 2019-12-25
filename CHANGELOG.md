69d3c42a434115e1f67cf5b285b3abdd4add2f10
Harshit Mahajan <hmahajan1999@gmail.com>
2019-12-25 17:31:24 +0530

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Change Log
+
+## [1.0.0] 2018-06-26
+### Original Release
+- Added Reactstrap as base framework
+- Added design from Paper Kit 2 by Creative Tim
---
+++ src/index.js
@@ -0,0 +1,59 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+// styles
+import "assets/css/bootstrap.min.css";
+import "assets/scss/paper-kit.scss";
+import "assets/demo/demo.css";
+// pages
+import MainPage from "views/MainPage.js";
+import NucleoIcons from "views/NucleoIcons.js";
+import LandingPage from "views/examples/LandingPage.js";
+import ProfilePage from "views/examples/ProfilePage.js";
+import RegisterPage from "views/examples/RegisterPage.js";
+// others
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/home" render={props => <MainPage {...props} />} />
+      <Route
+        path="/nucleo-icons"
+        render={props => <NucleoIcons {...props} />}
+      />
+      <Route
+        path="/landing-page"
+        render={props => <LandingPage {...props} />}
+      />
+      <Route
+        path="/profile-page"
+        render={props => <ProfilePage {...props} />}
+      />
+      <Route
+        path="/register-page"
+        render={props => <RegisterPage {...props} />}
+      />
+      <Redirect to="/home" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
