22ccf7b8d9462eb61312a40dd2cea2e692be5c1a
Harshit Mahajan <hmahajan1999@gmail.com>
2019-12-30 21:56:42 +0530

    deploy x

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));heroku 
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html')); 
   });
 }
 
